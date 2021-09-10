5f00488307385bc3d460f9fab52d79b04d6333bc
Rangeclone <rangeclone2332@gmail.com>
2021-09-10 22:12:35 +0100

    Update

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,27 +1,32 @@
-const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, token } = require('./config.json');
+const { token } = require('./config.json');
+const fs = require('fs');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// Place your client and guild ids here
+const clientId = '123456789012345678';
+const guildId = '876543210987654321';
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
-
 const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
 	try {
+		console.log('Started refreshing application (/) commands.');
+
 		await rest.put(
 			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
-		console.log('Successfully registered application commands.');
+		console.log('Successfully reloaded application (/) commands.');
 	}
 	catch (error) {
 		console.error(error);
