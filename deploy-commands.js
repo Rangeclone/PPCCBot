73c161405d440617c495858474c331432ec6bbbb
Rangeclone <rangeclone2332@gmail.com>
2021-09-10 22:13:50 +0100

    Fixed client id storage

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,14 +1,11 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { token } = require('./config.json');
+const { clientId, token } = require('./config.json');
 const fs = require('fs');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// Place your client and guild ids here
-const clientId = '123456789012345678';
-const guildId = '876543210987654321';
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
