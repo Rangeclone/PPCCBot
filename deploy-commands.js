bc3b2fc66477d07f1e0590cac36ba78ee45f9cac
Rangeclone <rangeclone2332@gmail.com>
2021-09-10 22:38:24 +0100

    commit

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,25 +1,26 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, token } = require('./config.json');
+const config = require('./config.json');
 const fs = require('fs');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+// Place your client and guild ids here
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(config.token);
 
 (async () => {
 	try {
 		console.log('Started refreshing application (/) commands.');
 
 		await rest.put(
-			Routes.applicationCommands(clientId),
+			Routes.applicationCommands(config.clientID),
 			{ body: commands },
 		);
 
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,18 @@
 module.exports = {
 	name: 'interactionCreate',
-	execute(interaction) {
-		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+	execute(client, interaction) {
+		if (!interaction.isCommand()) return;
+
+		const command = client.commands.get(interaction.commandName);
+
+		if (!command) return;
+
+		try {
+			command.execute(interaction);
+		}
+		catch (error) {
+			console.error(error);
+			interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
 	},
 };
