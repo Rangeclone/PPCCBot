4238d46cb0b9212255ffe00a89e48f97af73f809
Rangeclone <rangeclone2332@gmail.com>
2021-11-18 21:49:24 +0000

    e

--- rbxaccounthandler.js
+++ rbxaccounthandler.js
@@ -1,7 +1,6 @@
 const noblox = require('noblox.js');
 const config = require('./config.json');
 module.exports = {
-	// eslint-disable-next-line no-empty-function
 	async execute(guildId) {
 		if (config.groups && guildId) {
 			const selectedgroup = config.groups[String(guildId)];
