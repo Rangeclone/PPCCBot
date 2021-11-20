82ee04b588892e862270999a1295388d7b6376f7
Rangeclone <rangeclone2332@gmail.com>
2021-11-20 18:24:56 +0000

    Added support to mention users

--- commands/getrank.js
+++ commands/getrank.js
@@ -16,22 +16,17 @@ module.exports = {
 		users: [216703533460881409],
 	},
 	data: new SlashCommandBuilder()
-		.setName('rank')
-		.setDescription('Changes the rank of a ROBLOX user.')
+		.setName('getrank')
+		.setDescription('Gets the rank of a ROBLOX user.')
 		.addStringOption(option =>
 			option.setName('username')
 				.setDescription('Roblox Username')
-				.setRequired(true))
-		.addStringOption(option =>
-			option.setName('rank')
-				.setDescription('Name of the rank')
 				.setRequired(true)),
 	async execute(client, interaction) {
 
 		changeaccount(interaction.guild.id).then((response) => {
 			if (response == null) return interaction.reply({ content: 'Error: This guild has not been assigned a group/token.', components: [] });
 			if (response.success == false) return interaction.reply({ content: `Failed to login to roblox account: \`\`\`${response.error}\`\`\``, components: [] });
-			const rank = interaction.options.get('rank').value;
 			let username = interaction.options.get('username').value;
 			const member = userstringtouser(interaction, username);
 			if (member) {username = member.nickname || member.user.username;}
@@ -49,25 +44,22 @@ module.exports = {
 				if (!req) return interaction.reply({ content: 'Oops! We are currently have a problem communicating with roblox. <http://status.roblox.com/>', components: [] });
 				const req2 = JSON.parse(rbxresponse.body);
 				if (req2.success === false) return interaction.reply({ content: 'Failed to find ROBLOX user, did you enter the correct username?', components: [] });
-				noblox.getRoles(response.group).then((res44) => {
-					let roleid = null;
-					const searching = rank;
-					for (const i in res44) {
-						if (res44[i].name === searching) {
-							roleid = res44[i].rank;
-						}
+				noblox.getRankInGroup(response.group, Number(req2.Id)).then((rankid) => {
+					if (rankid) {
+						noblox.getRankNameInGroup(response.group, Number(req2.Id)).then((rankname) => {
+							if (rankname) {
+								interaction.reply({ content: `${username}'s rank: \`${rankname}\` \`${rankid}\``, components: [] });
+							}
+							else {
+								throw 'Did not recieve expected response from API.';
+							}
+						});
 					}
-					if (roleid == null) return interaction.reply({ content: 'Failed to find ROBLOX rank, did you enter the correct rank name?', components: [] });
-					noblox.setRank({ group: response.group, target: Number(req2.Id), rank: Number(roleid) }).then((nobloxres) => {
-						if (nobloxres) {
-							interaction.reply({ content: 'Successfully changed `' + username + '\'s` rank to `' + rank + '`', components: [] });
-						}
-						else {
-							throw 'Did not recieve expected response from API.';
-						}
-					}).catch(function(e) {
-						return interaction.reply({ content: `Failed to change \`${username}'s\` rank to \`${rank}\` \`\`\`${e}\`\`\``, components: [] });
-					});
+					else {
+						throw 'Did not recieve expected response from API.';
+					}
+				}).catch(function(e) {
+					return interaction.reply({ content: `Failed to find \`${username}'s\` rank. \`\`\`${e}\`\`\``, components: [] });
 				});
 			});
 		});
